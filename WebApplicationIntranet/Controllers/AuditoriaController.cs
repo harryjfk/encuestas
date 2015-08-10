b71100e3f0793850981238b8b95b3b37ffebbcfe
yenier <yenier.castaneda@gmail.com>
2015-08-10 15:07:09 -0500

    Sincronizacion10/08/2015

---
+++ WebApplicationIntranet/Controllers/AuditoriaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Domain;
+using Domain.Managers;
+using Entity;
+
+namespace WebApplication.Controllers
+{
+    public class AuditoriaController : BaseController<Auditoria>
+    {
+        public JsonResult Toggle(long id)
+        {
+            var manager = OwnManager;
+            var element = manager.Find(id);
+            if (element != null)
+            {
+               
+                manager.Modify(element);
+                manager.SaveChanges();
+            }
+            OwnManager.Get(Query);
+            var c = RenderRazorViewToString("_Table", Query);
+            var result = new
+            {
+                Success = true,
+                Data = c
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        
+    }
+}
