7ca1e1a905fa409abe1937eb33a3db97e94cec25
yenier <yenier.castaneda@gmail.com>
2015-08-17 16:26:58 -0500

    SelectsArreglados

--- WebApplicationIntranet/Scripts/Scripts.js
+++ WebApplicationIntranet/Scripts/Scripts.js
@@ -35,11 +35,21 @@
         $(el).html(title);
     },
     
-    search: function(element){
-        $(element).chosen({ disable_search_threshold: 10, no_results_text: "No se encontraron coincidencias con ", placeholder_text_single: "Seleccione elemento" });
-        $("div[class*='chosen-container chosen-container-single chosen-container-single-nosearch']").addClass("form-control");
-        var a = $("a[class*='chosen-single chosen-default']");
-        var a1 = $("a[class*='chosen-single']");
+    search: function (element) {
+        setTimeout(function () {           
+            $(element).chosen({ disable_search_threshold: 10, no_results_text: "No se encontraron coincidencias con ", placeholder_text_single: "Seleccione un elemento" });
+        $("div[class*='chosen-container chosen-container-single']").each(function () { $(this).addClass("form-control"); });
+        var a = $("a[class*='chosen-single']");
+        if (a != null) {
+            a.css("border", "0");
+            a.css("background", "white");
+            a.css("box-shadow", "0 0 0 white inset,0 0 0 rgba(0,0,0,.1)");
+        }
+    },1000);
+        //$(element).chosen({ disable_search_threshold: 10, no_results_text: "No se encontraron coincidencias con ", placeholder_text_single: "Seleccione elemento" });
+        //$("div[class*='chosen-container chosen-container-single chosen-container-single-nosearch']").addClass("form-control");
+        //var a = $("a[class*='chosen-single chosen-default']");
+        //var a1 = $("a[class*='chosen-single']");
         //if (a != null) {
         //    a.css("border", "0");
         //    a.css("background", "white");
