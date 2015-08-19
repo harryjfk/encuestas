0d34b841e3f0a1a1b44bf2aa189459a6092bfa9d
Hector <hectordavila64@gmail.com>
2015-08-19 16:20:40 -0500

    comenzando reporte Porcentaje de encuestas estadisticas

---
+++ Entity/Reportes/PorcentajeEncuestaEstadistica.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Reportes
+{
+    public class PorcentajeEncuestaEstadistica
+    {
+        public IEnumerable<PorcentajeEncuestaEstadisticaItem> Elements { get; set; }
+    }
+}
---
+++ Entity/Reportes/PorcentajeEncuestaEstadisticaItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity.Reportes
+{
+    public class PorcentajeEncuestaEstadisticaItem
+    {
+        public string Analista { get; set; }
+        public long IdAnalista { get; set; }
+        public IList<MonthData> Month { get; set; }
+        public IList<CiiuData> Ciius { get; set; }
+
+       
+    }
+
+    public class MonthData
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class CiiuData
+    {
+        public string Name { get; set; }
+        public long Id { get; set; }
+        public IList<MonthData> Month { get; set; }
+    }
+
+
+}
